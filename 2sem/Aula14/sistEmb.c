9fcd01766fe65fd929734d162daf9c4d9bae9a8c
IGamerBrasil <l.candemil@edu.pucrs.br>
2023-05-09 10:59:18 -0300

    CCCCC

--- 2sem/Aula14/sistEmb.c
+++ 2sem/Aula14/sistEmb.c
@@ -2,7 +2,16 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#define ON 1
+#define OFF 0
 
+void ledLight(int valor, int pos){
+  if(valor == OFF){
+    PORTB &= ~(1 << pos);  // desliga o led
+  }
+  else
+    PORTB |= 1 << pos;     // liga o led, pode modificar o pino apenas pondo em um reg PORT
+}
 
 int main (void) 
 {
@@ -10,12 +19,21 @@ int main (void)
 
   int periodo = 1000;
   int perSec = periodo/4;
+  int status = OFF;
+  ledLight(OFF, PB5); //comeca em 0
+
+  int countTime = 0;
 
   while(1)
   {
-    PORTB |= 1 << PB5;     // liga o led, pode modificar o pino apenas pondo em um reg PORT
-    _delay_ms(perSec);
-    PORTB &= ~(1 << PB5);  // desliga o led
-    _delay_ms(perSec);
+
+    _delay_ms(1);
+    ++countTime;
+
+    if(countTime >= perSec){ //momento de mudar o status do led
+      status = (status==OFF?ON:OFF);
+      ledLight(status, PB5);
+      countTime = 0;
+    }
   }
 }
